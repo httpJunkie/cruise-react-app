d0d325cb44bf4983101506db2ca08f90c2a891e1
Eric Bishard <e.bishard@gmail.com>
2018-08-06 20:00:18 -0700

    ensure clicking on title goes to reddit page, not media url

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Card } from 'semantic-ui-react';
 
-const Post = ({ url, title, author }) => (
-  <Card href={url} style={{ width: '100%' }}>
+const Post = ({ permalink, title, author }) => (
+  <Card href={'https://www.reddit.com/' + permalink} style={{ width: '100%' }}>
     <Card.Content>
       <Card.Header>{title}</Card.Header>
       <Card.Meta>posted by: {author}</Card.Meta>
